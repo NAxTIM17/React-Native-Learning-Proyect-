9d946a3807dc7ead6c24ec467a18db70fbc8b498
NAxTIM17 <naxtim15@gmail.com>
2025-02-05 15:03:46 -0300

    initialized project

---
+++ components/Card.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function Card() {
+  return (
+    <View>
+      <Text className="bg-red-300 p-2 rounded-md">
+        hola soy el componentes Card
+      </Text>
+    </View>
+  );
+}
